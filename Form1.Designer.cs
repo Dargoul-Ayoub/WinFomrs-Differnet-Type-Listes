beb7996df7dfda21b43ee3a2fc661d87ece67a14
infoayoub <infoayoub@github.com>
2021-02-25 15:24:56 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DiffernetLystesListes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(590, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(338, 65);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(168, 169);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(62, 290);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Selection Multiple";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0 élément(s) coché(s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(599, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "les Mois Sélectionés";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(590, 201);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 169);
+            this.textBox1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(30, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 173);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiffernetLystesListes
+{
+    public partial class Form1 : Form
+    {
+        ArrayList list_ItemSelected = new ArrayList();
+        ArrayList list_OneItem = new ArrayList();
+        ArrayList list_of_month = new ArrayList() { "Janvier","Féverier","Mars","Avril" };
+        string[] month1 = { "Mai", "Juin", "Juillet","Aout"};
+         string[] month2 = new string[] { "Septembre","Octobre", "Novembre", "Decembre" };
+
+       // List<string> lis = new List<string> { "1", "2" };
+        public Form1()
+        {
+            // listBox1.Items.AddRange(lis); il ne peut pas accepter list et arraylist
+
+            InitializeComponent();
+            listBox1.DataSource = list_of_month;
+            checkedListBox1.Items.AddRange(month1);
+            comboBox1.DataSource = month2;
+            textBox1.Text = string.Empty;
+            listBox1.ClearSelected();
+            
+            label1.Text = "0 Element(s) Coché(s)";
+
+        }
+        bool selected_multiple = false;
+        string iteme = null;
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+              selected_multiple = true;
+            else
+               selected_multiple = false;
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           textBox1.Text = string.Empty;
+            int nbr_ElementCoche=0;
+
+            if (!selected_multiple)
+            {
+                list_OneItem.Clear();
+                list_ItemSelected.Clear(); // i did it because i want when i selected an item , the only one shown in textBox is him
+
+                listBox1.SelectionMode = SelectionMode.One;
+                  iteme = Convert.ToString(listBox1.SelectedItem);
+                if (iteme != null)
+                    list_OneItem.Add(iteme);
+                nbr_ElementCoche++;
+
+            }
+            else
+            {
+                list_ItemSelected.Clear();
+                list_OneItem.Clear();
+                listBox1.SelectionMode = SelectionMode.MultiSimple;
+                foreach (var item in listBox1.SelectedItems)
+                {
+
+                    list_ItemSelected.Add(item);
+                    nbr_ElementCoche++;
+                }
+                list_OneItem.AddRange(list_ItemSelected);
+
+            }
+
+            
+
+            
+            foreach (var item in list_OneItem)
+                textBox1.Text += Convert.ToString(item) + Environment.NewLine;
+
+            label1.Text = nbr_ElementCoche.ToString()+" Element(s) Coché(s)";
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+                foreach (var item in list_OneItem)
+                textBox1.Text += Convert.ToString(item) + Environment.NewLine;
+            foreach (var item in checkedListBox1.CheckedItems)
+            textBox1.Text += Convert.ToString(item) +Environment.NewLine;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text += Convert.ToString(comboBox1.SelectedItem) + Environment.NewLine;
+        }
+    }
+}
